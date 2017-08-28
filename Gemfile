7f32ae3a203bd3422db542aa3390389b92b6570f
Eric-Guo <guochunzhong@bayekeji.com>
2017-08-28 14:56:25 +0800

    Add minitest ci

--- Gemfile
+++ Gemfile
@@ -43,5 +43,9 @@ group :development do
   gem 'web-console', '>= 3.3.0'
 end
 
+group :test do
+  gem 'minitest-ci'
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %I[mingw mswin x64_mingw jruby]
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,12 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/ci'
+
+if ENV['CIRCLECI']
+  Minitest::Ci.report_dir = "#{ENV['CIRCLE_TEST_REPORTS']}/reports"
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
