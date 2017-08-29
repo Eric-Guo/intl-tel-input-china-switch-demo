25e25e8894e5a3ab8e1ccb887d434634f441bd70
Eric-Guo <guochunzhong@bayekeji.com>
2017-08-29 13:28:26 +0800

    Add more assert

--- test/integration/home_test.rb
+++ test/integration/home_test.rb
@@ -4,5 +4,7 @@ class HomeTest < ApplicationSystemTestCase
   test 'loads correctly' do
     visit '/'
     assert page.has_content?('Example: Modify country data')
+    find(:css, '.flag-container').click
+    assert page.has_content?('United Kingdom')
   end
 end
